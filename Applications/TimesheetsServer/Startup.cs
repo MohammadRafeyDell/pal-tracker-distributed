5e48d290ffb9e61c872802d3ccd44079851febb6
mohammad_rafey@dell.com <mohammad_rafey@dell.com>
2019-08-01 18:44:25 +0530

    First distributed POC commit

--- Applications/TimesheetsServer/Startup.cs
+++ Applications/TimesheetsServer/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Steeltoe.Security.Authentication.CloudFoundry;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 
 namespace TimesheetsServer
 {
